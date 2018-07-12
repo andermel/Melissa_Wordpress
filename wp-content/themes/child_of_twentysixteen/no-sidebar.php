2b9bb6df16c57736ce47b657f80f11d627242da4
Melissa Anderson <ms.meander@gmail.com>
2018-07-11 23:13:05 -0400

    changed background color

--- wp-content/themes/child_of_twentysixteen/no-sidebar.php
+++ wp-content/themes/child_of_twentysixteen/no-sidebar.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * The template for displaying pages
+ * The template for displaying pages with no sidebar
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
+ * Template Name: no-sidebar.php
  *
  * @package WordPress
  * @subpackage Twenty_Sixteen
